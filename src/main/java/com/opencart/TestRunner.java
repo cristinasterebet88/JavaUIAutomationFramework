b2d8019528c18067c204393fdf887b8df297cbc7
csterebet <cristina_spac@yahoo.com>
2024-11-24 22:35:59 +0200

    Added the implementation of the register flow by using fake data and selenium web elements

--- src/main/java/com/opencart/TestRunner.java
+++ src/main/java/com/opencart/TestRunner.java
@@ -6,11 +6,14 @@ import org.openqa.selenium.*;
 
 public class TestRunner {
     public static void main(String[] args) throws InterruptedException {
-        //DriverManager.getInstance().getDriver().get("https://tekwillacademy-opencart.online/");
+        //DriverManager.getInstance().getDriver().get("https://www.google.com/");
         //Sau:
         WebDriver driver = DriverManager.getInstance().getDriver();
+        driver.get("https://www.google.com/");
+        System.out.println("The driver is on page: " + driver.getCurrentUrl());
+        System.out.println("The current page title: " + driver.getTitle());
 
-        String currentTabName = driver.getWindowHandle();
+        String theNameOfTheFirstTab = driver.getWindowHandle();
 
         //Open new window and navigate to Opencart page
         driver.switchTo().newWindow(WindowType.TAB);
@@ -28,10 +31,10 @@ public class TestRunner {
         System.out.println("The current URL is: " + driver.getCurrentUrl());
         System.out.println("The current page title: " + driver.getTitle());
 
-        WebElement firstNAmeInput = driver.findElement(By.id("input-firstname"));
-        firstNAmeInput.sendKeys(DataGeneratorManager.getRandomFirstName());
-        WebElement lastNAmeInput = driver.findElement(By.id("input-lastname"));
-        firstNAmeInput.sendKeys(DataGeneratorManager.getRandomLastName());
+        WebElement firstNameInput = driver.findElement(By.id("input-firstname"));
+        firstNameInput.sendKeys(DataGeneratorManager.getRandomFirstName());
+        WebElement lastNameInput = driver.findElement(By.id("input-lastname"));
+        lastNameInput.sendKeys(DataGeneratorManager.getRandomLastName());
 
         WebElement emailInput = driver.findElement(By.cssSelector("#input-email"));
         String emailData = DataGeneratorManager.getRandomPassword();
@@ -46,16 +49,16 @@ public class TestRunner {
         WebElement privacyToggleBar = driver.findElement(By.xpath("//input[@name='agree']"));
         ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", privacyToggleBar);
         Thread.sleep(500);
-
         privacyToggleBar.click();
 
         WebElement continueButton = driver.findElement(By.cssSelector("button[type='submit']"));
         continueButton.click();
-
         Thread.sleep(3000);
 
         //Close the current tab
         driver.close();
+        driver.switchTo().window(theNameOfTheFirstTab);
+        System.out.println("The current URL is: " + driver.getCurrentUrl());
         driver.quit();
     }
 }
